3465185664b5efe65465e5194ed3b5a6acdc8c1e
Rankyn Bass <rankyn@proton.me>
2025-02-16 19:56:50 -0800

    Add separate CI for valve-ntsync

--- wine-tkg-git/xiv-setup.sh
+++ wine-tkg-git/xiv-setup.sh
@@ -96,7 +96,6 @@ if [ "$xiv_valve" == "1" ]; then
     if [ "$xiv_ntsync" == "1" ]; then
         echo "Using ntsync valve patches. Known to work with commit b561e8d5d8a86062ca783296cb28ffe6e2be593"
         cp wine-tkg-userpatches/valvexbe/ntsync-latest.disabled wine-tkg-userpatches/ntsync-latest.mypatch
-        # rm -f wine-tkg-userpatches/proton-fshack-AMD-FSR-complete.mypatch
     fi
 else
     if [ "$xiv_staging" == "1" ]; then
