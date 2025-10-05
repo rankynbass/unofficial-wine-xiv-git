0b834dce34001937a070281654b7c13087942782
Rankyn Bass <rankyn@proton.me>
2025-10-05 13:10:15 -0700

    Fix install-headers for fedora container?

--- ntsync-header/install-header.sh
+++ ntsync-header/install-header.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 if [ ! -e "/usr/include/linux/ntsync.h" ]; then
     echo "NTSync header is missing from /usr/include/linux. Installing..."
+    mkdir -p /usr/include/linux
     cp ntsync-header/ntsync.h /usr/include/linux/ntsync.h
 fi
 echo "NTSync header is installed at /usr/include/linux/ntsync.h"
