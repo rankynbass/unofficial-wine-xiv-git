8719bf8d5da32d89a7308e1a5ed94838abfb29fa
Rankyn Bass <rankyn@proton.me>
2025-10-05 12:02:52 -0700

    Set _use_ntsync="false"

--- wine-tkg-git/xiv-staging.sh
+++ wine-tkg-git/xiv-staging.sh
@@ -33,8 +33,9 @@ echo "Setting up environment for Wine-XIV build"
 rm -f wine-tkg-userpatches/*.mypatch
 rm -f wine-tkg-userpatches/*.myrevert
 
+sed -i "s/LOCAL_PRESET=\"\(.*\)\"/LOCAL_PRESET=\"\"/" customization.cfg
 sed -i 's/_NOLIB32="false"/_NOLIB32="wow64"/' wine-tkg-profiles/advanced-customization.cfg
-if [ "$xiv_lsteamclient" == "0" ]; then
+if [ "$xiv_protonify" == "0" ]; then
     sed -i "s/_protonify=\"\(.*\)\"/_protonify=\"false\"/" customization.cfg
     echo "Diasbling Protonify patchset"
 else
@@ -49,9 +50,9 @@ else
     echo "No staging version set. Using latest commit"
 fi
 sed -i "s/_use_staging=\"\(.*\)\"/_use_staging=\"true\"/" customization.cfg
-sed -i "s/_use_esync=\"\(.*\)\"/_use_esync="false"/" customization.cfg
-sed -i "s/_use_fsync=\"\(.*\)\"/_use_fsync="false"/" customization.cfg
-sed -i "s/_use_ntsync=\"\(.*\)\"/_use_ntsync="true"/" customization.cfg
+sed -i "s/_use_esync=\"\(.*\)\"/_use_esync=\"false\"/" customization.cfg
+sed -i "s/_use_fsync=\"\(.*\)\"/_use_fsync=\"false\"/" customization.cfg
+sed -i "s/_use_ntsync=\"\(.*\)\"/_use_ntsync=\"false\"/" customization.cfg
 
 for f in wine-tkg-userpatches/staging/*.patch; do cp "$f" "wine-tkg-userpatches/$(basename ${f%.patch}).mypatch"; done
 if [ "$xiv_lsteamclient" == "0" ]; then
@@ -62,6 +63,7 @@ if [ "$xiv_lsteamclient" == "0" ]; then
     sed -i 's/pkgname=wine-tkg$/pkgname=unofficial-wine-xiv-nolsc/' non-makepkg-build.sh
     sed -i 's/pkgname=unofficial-wine-xiv$/pkgname=unofficial-wine-xiv-nolsc/' non-makepkg-build.sh
 else
+    echo "Enabling lsteamclient patches and binaries"
     sed -i 's/_lsteamclient_patches="\(.*\)"/_lsteamclient_patches="true"/' customization.cfg
     sed -i 's/_lsteamclient_patches="\(.*\)"/_lsteamclient_patches="true"/' wine-tkg-profiles/wine-tkg-valve-exp-bleeding.cfg
     sed -i 's/pkgname=wine-tkg$/pkgname=unofficial-wine-xiv/' non-makepkg-build.sh
